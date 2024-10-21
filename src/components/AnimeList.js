3675e05e778f51c2d55d1f3a620e2100eddc2d1d
Mrome <pavelmrome@gmail.com>
2024-10-21 23:30:20 +0700

    dsa

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './AnimeList.css';
 
+
+
 const AnimeList = () => {
-  const [animes, setAnimes] = useState([]);
+    const [animeList, setAnimeList] = useState([]);
 
   useEffect(() => {
     fetch('https://api.anilibria.tv/v3/title/updates?limit=10')
       .then(response => response.json())
       .then(data => {
-        Set.AnimeList(data.list); // допустим, у тебя есть setAnimeList для установки состояния
+        setAnimeList(data.list); // допустим, у тебя есть setAnimeList для установки состояния
       })
       .catch(error => console.error('Ошибка:', error));
   }, []);
@@ -17,7 +19,7 @@ const AnimeList = () => {
     <div className="anime-list">
       <h1>Anime List</h1>
       <div className="anime-grid">
-      {AnimeList.map(anime => (
+      {animeList.map(anime => (
   <div key={anime.id}>
     <h2>{anime.names.ru}</h2>
     <img src={`https://api.anilibria.tv${anime.posters.medium.url}`} alt={anime.names.ru} />
